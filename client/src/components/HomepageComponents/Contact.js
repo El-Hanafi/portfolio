d8f93c8256eb3c34be58433b5694e4c6df68e503
el-hanafi <m.h.benabbou@gmail.com>
2022-12-27 18:56:20 +0100

    codded the html for the contact section

--- client/src/components/HomepageComponents/Contact.js
+++ client/src/components/HomepageComponents/Contact.js
@@ -1,8 +1,44 @@
 import React from 'react';
 import './contact.css';
+import BackImg from '../../images/im4.jpg';
+import load1 from '../../images/load2.gif';
 
 export default function Contact() {
   return (
-    <div>Contact</div>
+    <div className="main-container">
+      <div className="contactForm">
+        <h2 className='title'>
+          contact form
+        </h2>
+
+        <div className="contactForm-center">
+          <div className="contact_form">
+            <form action="">
+
+              <p>message</p>
+
+              <label htmlFor="name">Name</label>
+              <input type="text" placeholder='import name...' required />
+
+              <label htmlFor="email">Email</label>
+              <input type="email" placeholder='import email...' required />
+
+              <label htmlFor="message">Message</label>
+              <textarea type='text' name="message" id="" placeholder='import contact reason'></textarea>
+
+              <div className="sen-btn">
+                <button type='submit'>Send</button>
+              </div>
+            </form>
+          </div>
+
+          {/* contact info  */}
+          <div className="contact-info">
+            <h4>Send your message</h4>
+            <img src={BackImg} alt=""/>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
