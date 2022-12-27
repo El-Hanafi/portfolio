b3bf265eb965799ee8ecdb53623617fc141bd346
el-hanafi <m.h.benabbou@gmail.com>
2022-12-27 18:45:54 +0100

    codded and styled up to the experience section

--- client/src/components/HomepageComponents/Education.js
+++ client/src/components/HomepageComponents/Education.js
@@ -2,6 +2,34 @@ import React from 'react'
 
 export default function Education() {
   return (
-    <div>Education</div>
+    <div className='main-container'>
+      <div className="education">
+
+        <h2 className='title'>Education</h2>
+
+        <div className="education-center">
+          
+          {/*single education*/}
+          <div className="single-education">
+            <p>CPGE</p>
+          </div>
+          {/*single education*/}
+          <div className="single-education">
+            <p>CPGE</p>
+          </div>
+          {/*single education*/}
+          <div className="single-education">
+            <p>CPGE</p>
+          </div>
+          {/*single education*/}
+          <div className="single-education">
+            <p>CPGE</p>
+          </div>
+        
+        </div>  
+        
+
+      </div>
+    </div>
   )
 }
--- client/src/components/HomepageComponents/Experience.js
+++ client/src/components/HomepageComponents/Experience.js
@@ -1,7 +1,35 @@
-import React from 'react'
+import React from 'react';
 
 export default function Experience() {
   return (
-    <div>Experience</div>
+    <div className='main-container'>
+      <h2 className='title'>
+        Experience
+      </h2>
+      <div className='experience'>
+        <div className="experience-center">
+
+          {/* static single experience */}
+          <div className="single-experience">
+            <p>Self Employed</p>
+          </div>
+          
+          {/* static single experience */}
+          <div className="single-experience">
+            <p>Self Employed</p>
+          </div>
+
+          {/* static single experience */}
+          <div className="single-experience">
+            <p>Self Employed</p>
+          </div>
+
+          {/* static single experience */}
+          <div className="single-experience">
+            <p>Self Employed</p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
