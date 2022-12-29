747c772b57def0534ea36e1a87633ce710451be7
el-hanafi <m.h.benabbou@gmail.com>
2022-12-29 01:25:10 +0100

    finished the education part of the admin section

--- client/src/components/AdminComponents/EducationAdmin.js
+++ client/src/components/AdminComponents/EducationAdmin.js
@@ -1,7 +1,37 @@
-import React from 'react'
+import React from 'react';
+import {Link} from 'react-router-dom';
 
 export default function EducationAdmin() {
   return (
-    <div>EducationAdmin</div>
+    <div className='same-component'>
+      <div class="same-form">
+        <form>
+          <h4>Education Component</h4>
+          <label htmlFor="text">Education</label>
+          <input type="text"  />
+          <button type="submit">Add item</button>
+        </form>
+      </div>
+
+      <div className="same-item">
+        <div className="about-info">
+          {/* single education */}
+          <div class="same-admin">
+            <div class="icons">
+              <Link to={"/editEducation"}>
+              
+              <i className="fas fa-edit"></i>
+            </Link>
+            <i class="fas fa-trash"></i>
+            </div>
+
+            <div class="single-education">
+              <p>web developper</p>
+            </div>
+            <h3 className='item-deleted-tab'>the message</h3>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
